f27b2c9e55c92628cc01243b2a8abce9206d82ca
Giorgio Balconi <giobt@hotmail.com>
2017-08-17 10:11:50 -0600

    first revision of custom chef installation recipe

--- elstkibn/recipes/default.rb
+++ elstkibn/recipes/default.rb
@@ -2,4 +2,22 @@
 # Cookbook:: elstkibn
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2017, Giorgio Balconi, All Rights Reserved.
+
+# Create RPM repository file in /etc/yum.repos.d/
+yum_repository 'elasticsearch-5.x' do
+  description "Elasticsearch repository for 5.x packages"
+  baseurl "https://artifacts.elastic.co/packages/5.x/yum"
+  gpgcheck true
+  gpgkey 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
+  enabled true
+  action :create
+end
+
+# Installing from the RPM repository
+yum_package 'elasticsearch'
+
+# Configure elasticsearch service to be enabled at boot and start it
+service "elasticsearch" do
+  action [:enabled, :start]
+end
